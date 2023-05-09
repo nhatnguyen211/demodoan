ef700493ee8d378c1ec5718923c3121b404b8150
nhatnguyen211 <nhatnguyenabs211@gmail.com>
2023-05-09 07:34:57 +0700

    first commit

---
+++ BELibrary/Migrations/202305080417439_AddTypeToMedicineTable1.cs
@@ -0,0 +1,18 @@
+﻿namespace BELibrary.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddTypeToMedicineTable1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Medicine", "Type", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Medicine", "Type");
+        }
+    }
+}
